cb65443c147d4ce2b772269dbf27ffc5b6e93802
EthanBraun <braune@oregonstate.edu>
2018-02-01 22:39:49 -0800

    Made pretty print prettier

--- Assignment2/MiniLogo.braune.hs
+++ Assignment2/MiniLogo.braune.hs
@@ -1,49 +1,149 @@
 module MiniLogo where
-import Prelude hiding (Num, Var)
 
+import Prelude hiding (Num)
+
+
+--
+-- * Syntax of MiniLogo
+-- Authors: Sam Cooney  - cooneys
+--          Ethan Braun - braune
+--
+
+-- Grammar for MiniLogo:
+-- num   ::= (any natural number)
+-- var   ::= (any variable name)
+-- macro ::= (any macro name)
+-- 
+-- prog  ::= ε | cmd ; prog 
+--
+-- mode  ::= down | up
+--
+-- expr  ::= var
+--        |  num
+--        |  expr + expr
+--
+-- cmd   ::= pen mode
+--        |  move ( expr , expr )
+--        |  define macro ( var* ) { prog }
+--        |  call macro ( expr* )
+--
+
+--
 -- Task 1
+--
+-- Encode the grammar as a set of Haskell data types
+--
+
 type Num = Int
-type Var = String
+
+type Var = [Char]
+
 type Macro = [Char]
 
-type Prog = [Cmd]
+--
+-- Pen status, down is drawing, up is not drawing
+--
 
 data Mode = Down | Up
+  deriving (Eq,Show)
 
-data Expr = Str Var
+data Expr = Vari Var
           | Numb Num
+          | Add Expr Expr
+  deriving (Eq,Show)
 
 data Cmd = Pen Mode
          | Move Expr Expr
-         | Define Macro [Expr] Prog
+         | Define Macro [Var] Prog
          | Call Macro [Expr]
+  deriving (Eq,Show)
 
+type Prog = [Cmd]
 
+--
 -- Task 2
-
+--
 -- Concrete MiniLogo syntax:
-
+--
 -- define line (x1, y1, x2, y2) {
 --    pen up; move (x1, y1)
 --    pen down; move (x2, y2)
 --    pen up;
 -- }
-
+--
+-- Define a MiniLogo macro line (x1,y1,x2,y2) that (starting from anywhere on the canvas) 
+-- draws a line segment from (x1,y1) to (x2,y2)
+--
 -- Abstract MiniLogo syntax:
+--
+
 line :: Cmd
-line = Define "line" [Str "x1", Str "y1", Str "x2", Str "y2"] [Pen Up, Move (Str "x1") (Str "y1"), Pen Down, Move (Str "x2") (Str "y2"), Pen Up]
+line = Define "line" ["x1","y1","x2","y2"] [Pen Up, Move (Vari "x1") (Vari "y1"), Pen Down, Move (Vari "x2") (Vari "y2"), Pen Up]
 
-
+--
 -- Task 3
-
+--
 -- Concrete MiniLogo syntax:
-
+--
 -- define nix (x, y, w, h) {
 --    line(x, y, x + w, y + h)
 --    line(x, y + h, x + w, y)
 -- }
-
+-- Use the line macro you just defined to define a new MiniLogo macro nix (x,y,w,h) that 
+-- draws a big “X” of width w and height h, starting from position (x,y). Your definition 
+-- should not contain any move commands.
+--
 -- Abstract MiniLogo syntax
-nix :: Cmd
-nix = Define "nix" [Str "x", Str "y", Str "w", Str "h"] [Call "line" [Str "x", Str "y", Str "x + w", Str "y + h"], Call "line" [Str "x", Str "y + h", Str "x + w", Str "y"]]
+--
 
+nix :: Cmd
+nix = Define "nix" ["x", "y", "w", "h"] [Call "line" [Vari "x", Vari "y", Add (Vari "x") (Vari "w"), Add (Vari "y") (Vari "h")], Call "line" [Add (Vari "x") (Vari "h"), Vari "y", Vari "x", Add (Vari "y") (Vari "h")]]
+
+--
+-- Task 4
+--
+-- Define a Haskell Function steps :: Int -> Prog that constructs a  MiniLogo program that
+-- draws a staircase of n steps startign from (0, 0). 
+--
+
+steps :: Int -> Prog
+steps 0 = []
+steps i = Call "line" [Numb i, Numb i, Numb (i-1), Numb i] : Call "line" [Numb (i-1), Numb i, Numb (i-1), Numb (i-1)] : steps (i-1)
+
+--
+-- Task 5
+--
+-- Define a haskell function macros :: Prog -> [Macro] that returns a list of all the names
+-- Of all the macros that are define anywhere in a given MiniLogo program.
+--
+
+macros :: Prog -> [Macro]
+macros []                 = []
+macros (Pen _ : t)        = macros t
+macros (Call _ _ : t)     = macros t
+macros (Move _ _ : t)       = macros t
+macros (Define m _ _ : t) = m : macros t
+
+--
+-- Task 6
+--
+-- Define a haskell function pretty :: prog -> string  that pretty prints a minilogo program.
+-- Transform the abstract syntax into concrete syntax
+--
+
+prettyExpr :: [Expr] -> String
+prettyExpr []           = ""
+prettyExpr (Vari v: t)  = v ++ prettyExpr t
+prettyExpr (Numb n: t)  = show n ++ " " ++ prettyExpr t
+prettyExpr (Add e x: t) = "( " ++ prettyExpr [e] ++ "+ " ++ prettyExpr [x] ++ ")" ++ prettyExpr t
+
+prettyVar :: [Var] -> String
+prettyVar (v : []) = v
+prettyVar (v : t) = v ++ ", " ++ prettyVar t
+
+pretty :: Prog -> String
+pretty []                 = "\n"
+pretty (Pen m : t)        = "\n   pen " ++ show m ++ ";" ++ pretty t
+pretty (Call m x : t)     = "\n   call " ++ m ++ " " ++ prettyExpr x ++ ";" ++ pretty t
+pretty (Move e x : t)     = "\n   move " ++ "(" ++ prettyExpr [e] ++ ", " ++ prettyExpr [x] ++ ");" ++ pretty t
+pretty (Define m v p : t) = "\ndefine " ++ m ++ " (" ++ prettyVar v ++ ") { " ++  pretty p ++ "} " ++ pretty t
