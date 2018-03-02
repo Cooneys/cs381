504a27c43d189a7865fef17385bb9620c9550ef7
EthanBraun <braune@oregonstate.edu>
2018-03-01 19:28:22 -0800

    Fixed merge conflicts

--- Assignment4/KarelSemantics.hs
+++ Assignment4/KarelSemantics.hs
@@ -19,20 +19,20 @@ test Empty      _ r = isEmpty r
 stmt :: Stmt -> Defs -> World -> Robot -> Result
 stmt Shutdown      _ _ r = Done r
 stmt Move          _ w r = let f = neighbor (getFacing r) (getPos r) 
-                        in if isClear f w
-                              then OK w (setPos f r)  
+                           in if isClear f w
+                              then OK w (setPos f r) 
                               else Error ("World is blocked at: " ++ show f)
 stmt PickBeeper    _ w r = let p = getPos r
-                        in if hasBeeper p w
-                              then OK (decBeeper p w) (incBag r)
-                              else Error ("No beeper to pick at: " ++ show p)
+                           in if hasBeeper p w
+                                 then OK (decBeeper p w) (incBag r)
+                                 else Error ("No beeper to pick at: " ++ show p)
 stmt PutBeeper     _ w r = let e = isEmpty r
-                            f = hasBeeper (getPos r) w
-                        in case (e, f) of
-                             (False, False) -> OK (incBeeper (getPos r) w) (decBag r)
-                             (False, True)  -> Error ("Beeper already at: " ++ show (getPos r))
-                             (True, False)  -> Error ("Out of beepers")
-                             (True, True)   -> Error ("Out of beepers and beeper already at: " ++ show (getPos r)) 
+                               f = hasBeeper (getPos r) w
+                           in case (e, f) of
+                                (False, False) -> OK (incBeeper (getPos r) w) (decBag r)
+                                (False, True)  -> Error ("Beeper already at: " ++ show (getPos r))
+                                (True, False)  -> Error ("Out of beepers")
+                                (True, True)   -> Error ("Out of beepers and beeper already at: " ++ show (getPos r))
 stmt (Turn d)      _ w r = OK w (setFacing (cardTurn d (getFacing r)) r) 
 stmt (Call m)      d w r = case lookup m d of
                              (Just s) -> stmt s d w r
@@ -45,6 +45,9 @@ stmt (If t s p)    d w r = if test t w r
                               else stmt p d w r
 stmt (While t s)   d w r = if test t wr
                               then stmt s d w r
+stmt (Block x)     d w r = case x of
+                             (h:t)  -> let 
+                             (h:[]) -> stmt h d w r
 -- | Run a Karel program.
 prog :: Prog -> World -> Robot -> Result
 prog (m,s) w r = stmt s m w r
