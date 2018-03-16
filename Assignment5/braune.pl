b17beb78792e57ed8808f30e2171a1594b280d05
EthanBraun <braune@oregonstate.edu>
2018-03-15 18:55:26 -0700

    Implemented part 1 of the assignment

--- Assignment5/braune.pl
+++ Assignment5/braune.pl
@@ -50,35 +50,57 @@ parent(selma,ling).
 %%
 
 % 1. Define a predicate `child/2` that inverts the parent relationship.
-
+child(X, Y) :- parent(Y, X).
 
 % 2. Define two predicates `isMother/1` and `isFather/1`.
-
+isMother(X) :- parent(X, _), female(X).
+isFather(X) :- parent(X, _), male(X).
 
 % 3. Define a predicate `grandparent/2`.
+grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
 
 
 % 4. Define a predicate `sibling/2`. Siblings share at least one parent.
+sibling(X, Y) :- parent(Z, X), parent(Z, Y), (X \= Y).
 
 
 % 5. Define two predicates `brother/2` and `sister/2`.
-
+brother(X, Y) :- sibling(X, Y), male(X).
+sister(X, Y) :- sibling(X, Y), female(X).
 
 % 6. Define a predicate `siblingInLaw/2`. A sibling-in-law is either married to
 %    a sibling or the sibling of a spouse.
+siblingInLaw(X, Y) :- married(X, Z), sibling(Z, Y).
+siblingInLaw(X, Y) :- married(Y, Z), sibling(Z, X).
 
 
 % 7. Define two predicates `aunt/2` and `uncle/2`. Your definitions of these
 %    predicates should include aunts and uncles by marriage.
+aunt(X, Y) :- sister(X, Z), parent(Z, Y). 
+aunt(X, Y) :- female(X), siblingInLaw(X, Z), parent(Z, Y). 
+uncle(X, Y) :- brother(X, Z), parent(Z, Y).
+uncle(X, Y) :- male(X), siblingInLaw(X, Z), parent(Z, Y). 
 
 
 % 8. Define the predicate `cousin/2`.
+cousin(X, Y) :- child(X, Z), aunt(Z, Y). 
+cousin(X, Y) :- child(X, Z), uncle(Z, Y). 
 
 
 % 9. Define the predicate `ancestor/2`.
+%ancestor_(X, Y) :- parent(X, Y).
+%ancestor(X, Y) :- ancestor_(X, Y).
+%ancestor(X, Y) :- ancestor_(X, Z), ancestor_(Z, Y).
+ancestor(X, Y) :- parent(X, Y).
+ancestor(X, Y) :- parent(X, Z), ancestor(Z, Y).
 
 
 % Extra credit: Define the predicate `related/2`.
+related(X, Y) :- parent(X, Y).
+related(X, Y) :- aunt(X, Y).
+related(X, Y) :- uncle(X, Y).
+related(X, Y) :- ancestor_(X, Y).
+related(X, Y) :- ancestor_(X, Z), ancestor_(Z, Y).
 
 
 
